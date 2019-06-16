bb9615594b1b112ec4d9ecfc4ebc94ba553a12c8
Yosef Lehrer <yolehrer@gmail.com>
2019-06-16 16:53:34 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+
+
+def reverse_each_word(sentence)
+first_array = sentence.split(" ")
+final_array = []
+first_array.collect do |sentence|
+  final_array.push(sentence.reverse)
+  end
+  final_array.join(" ")
+end
