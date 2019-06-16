7162f547d8eba521feb30b58d9e8f6ebb31970c2
Yosef Lehrer <yolehrer@gmail.com>
2019-06-16 16:42:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(sentence)
 empty_array = []
 empty_array.push(sentence.split(" "))
 empty_array.collect do |word|
-  empty_array.reverse!
+  word.reverse!
   end
   empty_array
 end
